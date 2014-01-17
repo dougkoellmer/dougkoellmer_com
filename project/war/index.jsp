443ccc620c140e313e509ae995551b48d4a0691b
Doug Koellmer <dougk16@tlen.pl>
2014-01-16 23:03:39 -0800

    Removed caja from build step.

--- project/war/index.jsp
+++ project/war/index.jsp
@@ -13,7 +13,7 @@
 
 <link rel="shortcut icon" href="/favicon.ico?v=3" />
 
-<link type="text/css" rel="stylesheet" href="/r.app/min.css?v=1389894489" />
+<link type="text/css" rel="stylesheet" href="/r.app/min.css?v=1389941935" />
 
 <script type="text/javascript">
 var RecaptchaOptions={theme : 'custom'};
@@ -48,6 +48,6 @@ var RecaptchaOptions={theme : 'custom'};
 		</table>
 	</div>
 	
-	<script type="text/javascript" src="/r.app/min.js?v=1389894489"></script>
+	<script type="text/javascript" src="/r.app/min.js?v=1389941934"></script>
 </body>
 </html>
---
+++ scripts/client/build_js.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# "Builds" all the JS, taking minified GWT output, and cat-ing that with minified native support JS
+# like History.JS, CodeMirror, Modernizr, etc. Then updates resource version in HTML (or JSP) file.
+
+# Minify support libraries.
+SWARM_SCRIPTS="../..//project/lib/swarm/scripts/client"
+cd $SWARM_SCRIPTS
+sh ./minify_codemirror.sh
+sh ./minify_history.sh
+cd -
+
+# cat support libraries together into one dependency.
+SWARM_JS_DIR=../../project/lib/swarm/src/swarm/client/js
+CM_DIR="$SWARM_JS_DIR/codemirror"
+HISTORY_DIR="$SWARM_JS_DIR/history"
+MODERNIZR_DIR="$SWARM_JS_DIR/modernizr"
+TEMP_OUT_DIR="./temp"
+cat $HISTORY_DIR/native.history_min.js $MODERNIZR_DIR/modernizr.custom.90450.js $CM_DIR/cm_min.js > $TEMP_OUT_DIR/dependencies.min.js
+
+# cat minified support and main app js bootstrapper into one file.
+APP_JS=$(realpath ../../project/war/r.app/r.app.nocache.js)
+JS_MIN_OUT=min.js
+SUPPORT_JS="$TEMP_OUT_DIR/dependencies.min.js"
+MODULE=$(realpath ../../project/war/r.app)
+cat $SUPPORT_JS $APP_JS > "$MODULE/$JS_MIN_OUT"
+
+# update the query string in the js link in the html in order to force-clear the cache.
+JSP=$(realpath ../../project/war/index.jsp)
+sh $SWARM_SCRIPTS/update_resource_version.sh $JSP $JS_MIN_OUT
