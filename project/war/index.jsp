ce9ee73794b50f833c945db02bb3d0f2e53b39a5
Doug Koellmer <dougk16@tlen.pl>
2014-02-03 12:42:56 -0800

    Updated resume.

--- project/war/index.jsp
+++ project/war/index.jsp
@@ -14,7 +14,7 @@
 
 <link rel="shortcut icon" href="/favicon.ico?v=3" />
 
-<link type="text/css" rel="stylesheet" href="/r.app/min.css?v=1391409977" />
+<link type="text/css" rel="stylesheet" href="/r.app/min.css?v=1391453225" />
 
 <script type="text/javascript">
 var RecaptchaOptions={theme : 'custom'};
@@ -55,6 +55,6 @@ var RecaptchaOptions={theme : 'custom'};
 		</table>
 	</div>
 	
-	<script type="text/javascript" src="/r.app/min.js?v=1391409976"></script>
+	<script type="text/javascript" src="/r.app/min.js?v=1391453224"></script>
 </body>
 </html>
--- scripts/client/build_resume_portfolio.sh
+++ scripts/client/build_resume_portfolio.sh
@@ -5,6 +5,6 @@ java -jar ../../tools/htmlcompressor-1.5.3.jar --remove-intertag-spaces --compre
 java -jar ../../tools/htmlcompressor-1.5.3.jar --remove-intertag-spaces --compress-css --compress-js -t html -o ../../project/war/portfolio/index.html ../../project/war/portfolio/index_debug.html
 
 #! Need to change --page-height if I add/remove any more stuff...ugh
-../../tools/wkhtmltopdf/wkhtmltopdf.exe --print-media-type -T 0mm -B 0mm -L 0mm -R 0mm --zoom 1.4 --page-width 150 --page-height 183.5 ../../project/war/resume/index.html ../../project/war/resume.pdf
+../../tools/wkhtmltopdf/wkhtmltopdf.exe --print-media-type -T 0mm -B 0mm -L 0mm -R 0mm --zoom 2.5 --page-width 260 --page-height 330 ../../project/war/resume/index.html ../../project/war/resume.pdf
 
 cp ../../project/war/resume.pdf ../../external/Resume_DougKoellmer.pdf
