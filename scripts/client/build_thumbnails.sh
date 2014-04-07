c6c301d7bc1358dcbf5edc01bf160053ca87e44f
Doug Koellmer <dougk16@tlen.pl>
2014-04-06 20:15:19 -0700

    Fixed minor resume tool tip bug.

--- scripts/client/build_thumbnails.sh
+++ scripts/client/build_thumbnails.sh
@@ -4,12 +4,15 @@ SEARCH_DIR="../../project/war/img/cell_content"
 
 for entry in "$SEARCH_DIR"/*
 do
-	if [[ $entry == *.splash.* ]]
+	if [[ $entry == *.splash.* -o $entry == *.strip_0.* ]]
 	then
 		thumb=${entry/splash/thumb}
 		thumb=${thumb/cell_content/"cell_content/thumbs"}
 		
 		convert.exe $entry -resize 64x64^ -gravity north -extent 64x64 -quality 100% $thumb
+	elif [[  ]]
+	then
+	
 	fi
 done
 
