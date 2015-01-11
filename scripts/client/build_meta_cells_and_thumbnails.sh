e36a6aed9f7ff565637b381e7378855d80305111
Doug Koellmer <dougk16@tlen.pl>
2015-01-11 15:01:06 -0800

    Small mod to meta_cell/thumb gen scripts.

--- scripts/client/build_meta_cells_and_thumbnails.sh
+++ scripts/client/build_meta_cells_and_thumbnails.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+source ./config.sh
+
 #Snapshots have to be take twice because thumb sprite plate has to be
 #compiled first before snapshots of thumb cells for meta so thumbs are actually there.
 sh build_thumbnails.sh take_snapshots
 sh build_meta_cells.sh take_snapshots
+
+rm -rf $SNAPSHOT_DIR
