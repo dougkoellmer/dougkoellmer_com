21d74531d9be39a14668965feebe2c66d197f618
Doug <doug@dougkoellmer.com>
2016-11-20 23:01:47 -0500

    tweaks.

--- scripts/client/build_meta_cells_and_thumbnails.sh
+++ scripts/client/build_meta_cells_and_thumbnails.sh
@@ -4,6 +4,7 @@ source ./config.sh
 
 #Snapshots have to be take twice because thumb sprite plate has to be
 #compiled first before snapshots of thumb cells for meta so thumbs are actually there.
+# YOU'LL notice errors referencing thumb plate not existing, cause on the first pass it doesn't
 sh build_thumbnails.sh take_snapshots
 sh build_meta_cells.sh take_snapshots
 
---
+++ scripts/client/rollback_deploy.sh
@@ -0,0 +1,5 @@
+source ./config.sh
+
+sh ${GAE_HOME}/bin/appcfg.sh --email=dougk16@tlen.pl rollback ../../project/war
+
+#"%~dp0\..\lib\appengine-tools-api.jar"
